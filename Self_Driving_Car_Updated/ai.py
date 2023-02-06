0c4db68a8b090346f933aefbfa80f3128a4e46bf
Mustafa Hasan <mustafahasan@Mustafas-Air.ht.home>
2023-02-06 16:17:23 -0500

    added comment

--- Self_Driving_Car_Updated/ai.py
+++ Self_Driving_Car_Updated/ai.py
@@ -6,7 +6,7 @@ import numpy as np
 import random
 import os
 import torch
-import torch.nn as nn
+import torch.nn as nn #neural netwrok
 import torch.nn.functional as F
 import torch.optim as optim
 import torch.autograd as autograd
